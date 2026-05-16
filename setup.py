add6b398239085446e4a897284669c8aa0397863
Xziantseo <Xziantseo@gmail.com>
2026-05-16 23:20:52 +0530

    eda changes

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Part",
+    author_mail="ankitagrawaljhansi1@gmail.com",
+    packages=find_packages()
+)
+
+
